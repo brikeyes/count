408ecc1c5e5298f2a7bb7092014dd5e8a4d51a71
brian k <bkeyes@vizuri.com>
2018-05-23 10:52:35 -0400

    Update app.py
    add while true statement

--- app.py
+++ app.py
@@ -1,4 +1,2 @@
-import sys
-for i in range(sys.maxsize**10):  # you could go even higher if you really want
-    if there_is_a_reason_to_break(i):
-        break
+while True :
+     print ("hello python ")
